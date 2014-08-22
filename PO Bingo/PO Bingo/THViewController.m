8998d24ad8462677cc91318beab1c9c8526e001c
Tim Herbig <tim.herbig@nb-therbig.local>
2014-08-22 22:56:55 +0200

    Added Array with first bunch of slogans

--- PO Bingo/PO Bingo/THViewController.m
+++ PO Bingo/PO Bingo/THViewController.m
@@ -27,18 +27,35 @@
 }
 
 - (IBAction)buttonPressed {
-    self.sloganLabel.text = @"YES";
+    
+    NSArray *slogans = [[NSArray alloc] initWithObjects:@"It can’t be that hard, right?",@"It’s probably an easy task.",@"I assume that it will look like this.",@"Let's launch with an MVP!",@"Mobile can be done later.",@"#YOLO",@"Do you really need acceptance criteria for this one?",@"Can't we just ship it without QA?",@"Looks good to me.",@"Who's working on this task?",@"I fear we have to pull this into the sprint.",@"Let's assume the designs were final.",@"I'll take care of the copy.",@"Sorry, but this is out of scope for the MVP.", nil];
+    
+    self.sloganLabel.text = [slogans objectAtIndex:3];
 }
 
 
-
-
-
-
-
-
-
-
-
-
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
