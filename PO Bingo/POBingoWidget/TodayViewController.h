ebd5666a9ac85d2c1a4619168ebb54c69979b092
Tim Herbig <timherbig@Tims-MacBook-Air.local>
2014-09-20 21:54:06 +0200

    Added additonal assets and Notification Center Widget

---
+++ PO Bingo/POBingoWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  POBingoWidget
+//
+//  Created by Tim Herbig on 20.09.14.
+//  Copyright (c) 2014 TimHerbig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
